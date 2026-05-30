68c2cd3192dbdf9102a4d8305cff600098c410ef
shosipaniti <283887905+shosipaniti@users.noreply.github.com>
2026-05-30 21:47:01 +0900

    Add machine learning predicted questions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "keizai-quiz-v19";
+const CACHE_NAME = "keizai-quiz-v20";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,7 +7,7 @@ const ASSETS = [
   "./app.js",
   "./questions.js",
   "./predicted_questions.js",
-  "./study_extra_questions.js?v=6",
+  "./study_extra_questions.js?v=7",
   "./manifest.webmanifest",
   "./icon.svg",
   "./examples/question-template.csv",
